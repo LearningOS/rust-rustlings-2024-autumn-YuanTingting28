cb17c17f37f439f43178fcbd69a982dc0a3c3440
Yuantingting28 <2536394486@qq.com>
2024-10-16 22:37:37 +0800

    113

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -15,19 +15,35 @@
 // "A+" to show that your changes allow alphabetical grades.
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
-
 // I AM NOT DONE
+use std::fmt;
 
-pub struct ReportCard {
-    pub grade: f32,
+// Define an enum to represent different types of grades
+#[derive(Debug)]
+
+enum Grade {
+    Numeric(f32),
+    Alphabetic(String),
+}
+
+impl fmt::Display for Grade {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Grade::Numeric(value) => write!(f, "{}", value),
+            Grade::Alphabetic(value) => write!(f, "{}", value),
+        }
+    }
+}
+
+pub struct ReportCard<T> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: fmt::Display> ReportCard<T> {
     pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+        format!("{} ({}) - achieved a grade of {}", &self.student_name, &self.student_age, &self.grade)
     }
 }
 
@@ -38,7 +54,7 @@ mod tests {
     #[test]
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Grade::Numeric(2.1),
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
@@ -50,9 +66,8 @@ mod tests {
 
     #[test]
     fn generate_alphabetic_report_card() {
-        // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Grade::Alphabetic("A+".to_string()),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
